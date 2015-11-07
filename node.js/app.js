5172eae1bc9db6d85ce4f873e760fb83d8b57ea9
Guillermo Valle <guillefix@gmail.com>
2015-11-07 16:24:14 +0100

    testing

--- node.js/app.js
+++ node.js/app.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
 var web3 = require('web3');
+var http = require('http');
 var io = require('socket.io')(http);
 
 var orderAbi=[{"constant":true,"inputs":[],"name":"status","outputs":[{"name":"","type":"uint8"}],"type":"function"},{"constant":false,"inputs":[],"name":"OrderIsNew","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[],"name":"fileURL","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"customerID","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[],"name":"remove","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"OrderIsFinished","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[],"name":"printerID","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"_printerID","type":"uint256"}],"name":"isMyOrder","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":false,"inputs":[{"name":"_printerID","type":"uint256"}],"name":"acceptOrder","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"OrderIsPrinting","outputs":[{"name":"","type":"bool"}],"type":"function"},{"inputs":[{"name":"_customerID","type":"uint256"},{"name":"_fileURL","type":"string"}],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"printerID","type":"uint256"}],"name":"jobClaimed","type":"event"}];
 
-var factoryAbi  = [{"constant":false,"inputs":[{"name":"_customerID","type":"uint256"},{"name":"_fileURL","type":"string"}],"name":"createOrder","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_Order","type":"address"}],"name":"addOrder","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"numOrders","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"orders","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"latestOrder","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[],"name":"getNextNewOrder","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[],"name":"test","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"Order","type":"address"}],"name":"jobSubmitted","type":"event"}]
+var factoryAbi  = [{"constant":false,"inputs":[{"name":"_customerID","type":"uint256"},{"name":"_fileURL","type":"string"}],"name":"createOrder","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"_Order","type":"address"}],"name":"addOrder","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"numOrders","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"orders","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"latestOrder","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[],"name":"getNextNewOrder","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[],"name":"test","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"Order","type":"address"}],"name":"jobSubmitted","type":"event"}]
 
 
 
-var orderFactoryAddress = "0x89899572e17ea0390909e9673df92c1d76b6241b";
+var orderFactoryAddress = "0xfdb766f43f94e001976e6634a8eb5f25c12b4e7e";
 var orderFactoryContract = web3.eth.contract(factoryAbi);
 var orderFactory = orderFactoryContract.at(orderFactoryAddress);
 var event = orderFactory.jobSubmitted({Order: 23} );
@@ -30,7 +31,7 @@ res.send(bal);
 }
 
 function addOrder(customerID,fileURL){
-	orderFactory.createOrder.sendTransaction(customerID,fileURL, {from: eth.accounts[0],gas: 1000000});
+	orderFactory.createOrder.sendTransaction(customerID,fileURL, {from: web3.eth.accounts[0],gas: 1000000});
 
 }
 
@@ -43,8 +44,9 @@ function sendDesign(req, res) {
 	console.log(req.query.fileURL);
 
 	orderID = addOrder(req.query.customerID, req.query.fileURL);
-
+	sendGethData("Printing");
 	res.send(orderID);
+
 }
 
 
